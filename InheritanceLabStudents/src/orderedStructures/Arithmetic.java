2e47daeb85a6dc4d2b0d5f94a087ff9d834bfd1f
Correa22 <christ0522@hotmail.com>
2017-08-31 13:09:43 -0400

    Add files via upload

---
+++ InheritanceLabStudents/src/orderedStructures/Arithmetic.java
@@ -0,0 +1,30 @@
+package orderedStructures;
+
+public class Arithmetic extends Progression {
+	private double commonDifference; 
+	
+	public Arithmetic(double firstValue, double commonDifference) { 
+		super(firstValue); 
+		this.commonDifference = commonDifference; 
+	}
+	
+	@Override
+	public double nextValue() {
+		current = current + commonDifference; 
+		return current;
+	}
+	public double getTerm(int n){
+		double temp = super.firstValue();
+		if(n==1){
+			return temp;
+		}
+		for(int i=2; i<n; i++){
+			temp=this.nextValue();
+		}
+		return temp;
+	}
+	public String toString(){
+		return "Ari("+super.firstValue() + ", "+ this.nextValue()+ " )";
+	}
+
+}
---
+++ InheritanceLabStudents/src/orderedStructures/Geometric.java
@@ -0,0 +1,31 @@
+package orderedStructures;
+
+public class Geometric extends Progression {
+
+	private double commonFactor; 
+	
+	public Geometric(double firstValue, double commonFactor) { 
+		super(firstValue); 
+		this.commonFactor = commonFactor; 
+	}
+	
+	@Override
+	public double nextValue() {
+		current = current * commonFactor; 
+		return current;
+	}
+	public String toString(){
+		return "Geo("+super.firstValue() + ", "+ this.nextValue()+ " )";
+	}
+	public double getTerm(int n){
+		double temp = super.firstValue();
+		if(n==1){
+			return temp;
+		}
+		for(int i=2; i<n; i++){
+			temp=this.nextValue();
+		}
+		return temp;
+	}
+
+}
